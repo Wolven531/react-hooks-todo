61e10ab449e664375281a2f8a51832013aebc896
Anthony Williams <anthony.williams.cs@gmail.com>
2019-04-09 23:58:57 -0600

    Use new TodoModel in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
+import React, { useState } from 'react'
+
+import { TodoModel } from './TodoModel'
+
 import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    )
-  }
+const App = () => {
+	const [todos, setTodos] = useState<TodoModel[]>([
+		new TodoModel(0, 'todo task 1')
+	])
+	const [todo, setTodo] = useState('')
+
+	return (
+		<div className="app">
+			<h1>To Do</h1>
+		</div>
+	)
 }
 
 export default App
