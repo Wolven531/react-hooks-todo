a2213fb18b7d2881461afe223e5c9c8332e16716
Anthony Williams <anthony.williams.cs@gmail.com>
2019-04-21 22:18:22 -0600

    Start App tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import ReactDOM from 'react-dom'
+
+import { load } from 'cheerio'
+// import { shallow } from 'enzyme'
+
 import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+describe('Todo App component', () => {
+	let fixture: ReactElement
+	let parentDiv: HTMLElement
+
+	beforeAll(() => {
+		fixture = <App/>
+		parentDiv = document.createElement('div')
+		ReactDOM.render(fixture, parentDiv)
+	})
+
+	it('renders without crashing', () => {
+		const $ = load(parentDiv.outerHTML)
+		expect($('.app').length).toBe(1)
+	})
+
+	afterAll(() => {
+		ReactDOM.unmountComponentAtNode(parentDiv)
+	})
 })
