ae542cc1b4f405fae394c062abb8096abf70cc8c
Anthony Williams <anthony.williams.cs@gmail.com>
2019-04-23 21:09:46 -0600

    Remove semicolons after eject

--- config/jest/fileTransform.js
+++ config/jest/fileTransform.js
@@ -1,16 +1,16 @@
-'use strict';
+'use strict'
 
-const path = require('path');
+const path = require('path')
 
 // This is a custom Jest transformer turning file imports into filenames.
 // http://facebook.github.io/jest/docs/en/webpack.html
 
 module.exports = {
   process(src, filename) {
-    const assetFilename = JSON.stringify(path.basename(filename));
+    const assetFilename = JSON.stringify(path.basename(filename))
 
     if (filename.match(/\.svg$/)) {
-      return `const React = require('react');
+      return `const React = require('react')
       module.exports = {
         __esModule: true,
         default: ${assetFilename},
@@ -23,9 +23,9 @@ module.exports = {
             children: ${assetFilename}
           })
         })),
-      };`;
+      };`
     }
 
-    return `module.exports = ${assetFilename};`;
+    return `module.exports = ${assetFilename};`
   },
-};
+}
