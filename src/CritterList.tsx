31cba4e885f44fbeb60cc86dfe4d4f32f2760f05
Anthony Williams <anthony.williams.cs@gmail.com>
2019-05-07 21:55:15 -0600

    Refactor CritterList to use new components

--- src/CritterList.tsx
+++ src/CritterList.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect } from 'react'
 
+import { Critter } from './Critter'
+import { CritterListControls } from './CritterListControls'
+
 import { useCritterState } from './useCritterState'
 
 import './CritterList.css'
@@ -11,62 +14,11 @@ const CritterList = () => {
 		loadFromStorage()
 	}, [])
 
-	const toggleSelectedOnClick = (evt: React.MouseEvent) => {
-		if (evt.currentTarget.classList.contains('selected')){
-			evt.currentTarget.classList.remove('selected')
-			return
-		}
-		evt.currentTarget.classList.add('selected')
-	}
-
 	return (
 		<article className="critter-list">
-			<section className="controls">
-				<button className="create" onClick={() => { spawnCritter() }}>
-					Spawn Critter
-				</button>
-				<button className="update" onClick={() => { saveToLocalStorage() }}>
-					Save Critters (local)
-				</button>
-				<button className="delete" onClick={() => { clearCritters() }}>
-					Clear Critters
-				</button>
-			</section>
+			<CritterListControls clearCritters={clearCritters} saveToLocalStorage={saveToLocalStorage} spawnCritter={spawnCritter} />
 			<section className="display-container">
-				{critters.map(critter => {
-					// console.info(`[render | CritterList] Rendering id="${critter.id}"`)
-					return (
-						<section className="critter" key={critter.id} onClick={toggleSelectedOnClick}>
-							<table>
-								{/*
-								<thead>
-									<tr>
-										<td colSpan={2}>{critter.id}</td>
-									</tr>
-								</thead>
-								*/}
-								<tbody>
-									<tr>
-										<td>Name</td>
-										<td>{critter.name}</td>
-									</tr>
-									<tr>
-										<td>HP</td>
-										<td>{critter.hitpoints}</td>
-									</tr>
-									<tr>
-										<td>Attack</td>
-										<td>{critter.attack}</td>
-									</tr>
-									<tr>
-										<td>Defense</td>
-										<td>{critter.defense}</td>
-									</tr>
-								</tbody>
-							</table>
-						</section>
-					)
-				})}
+				{critters.map(critter => <Critter key={critter.id} critter={critter} />)}
 			</section>
 		</article>
 	)
