f1723edb3487755f586c4c6b124f41ca3e593a4e
Anthony Williams <anthony.williams.cs@gmail.com>
2019-05-10 21:20:49 -0600

    Update turn model to use Combatant model

--- src/model/Turn.ts
+++ src/model/Turn.ts
@@ -1,5 +1,12 @@
-import { Critter } from './Critter';
-import { Weapon } from './Weapon';
-export class Turn {
-	constructor(public fighter1: Critter, public fighter2: Critter, public weapon1: Weapon, public weapon2: Weapon) { }
+import { Combatant } from './Combatant'
+import { Weapon } from './Weapon'
+
+class Turn {
+	constructor(
+		public fighter1: Combatant,
+		public fighter2: Combatant,
+		public weapon1: Weapon,
+		public weapon2: Weapon) { }
 }
+
+export { Turn }
