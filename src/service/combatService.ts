61f12f373c248b12da94d9c920b0573906f2ff82
Anthony Williams <anthony.williams.cs@gmail.com>
2019-05-11 23:47:56 -0600

    Start attack for second combatant in combatService

--- src/service/combatService.ts
+++ src/service/combatService.ts
@@ -24,12 +24,15 @@ const combatService = () => {
 			while (!isOver(c1, c2)) {
 				const t = new Turn(c1, c2, weapon1, weapon2)
 				console.info(`[ fight | combatService] Creating turn ${JSON.stringify(t)}...`)
+				const damageAgainstC2 = getDamageForAttack(c1, weapon1)
+				const damageAgainstC1 = getDamageForAttack(c2, weapon2)
 				// NOTE: primary attack for turn
-				console.info(`${c1.name} attacks ${c2.name} with ${weapon1.name}...`)
-				const damage = getDamageForAttack(c1, weapon1)
-				c2.currentHitpoints -= damage
+				console.info(`[ fight | combatService] ${c1.name} attacks ${c2.name} with ${weapon1.name}...`)
+				c2.currentHitpoints -= damageAgainstC2
 				// NOTE: potential counterattack?
-				// TODO: add c2 attacking c1
+				// TODO: fix c2 attacking c1
+				console.info(`[ fight | combatService] ${c2.name} attacks ${c1.name} with ${weapon2.name}...`)
+				c1.currentHitpoints -= damageAgainstC1
 			}
 
 		return turns
