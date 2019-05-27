63841dd0b7f39eccb1f26283bf9d67f0bd8067ee
Anthony Williams <anthony.williams.cs@gmail.com>
2019-05-26 22:51:18 -0600

    Add gatherers to useMoneyState

--- src/state/useMoneyState.ts
+++ src/state/useMoneyState.ts
@@ -2,9 +2,14 @@ import { useState } from 'react'
 
 const useMoneyState = (initialValue: number) => {
 	const [money, setMoney] = useState(initialValue)
+	const [gatherers, setGatherers] = useState(0)
 
 	return {
+		gatherers,
 		money,
+		addGatherer: () => {
+			setGatherers(gatherers + 1)
+		},
 		addMoney: () => {
 			setMoney(money + 1)
 		}
