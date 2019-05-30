3263c0e7e18dc1698e6dc028d40211524e1144af
Anthony Williams <anthony.williams.cs@gmail.com>
2019-05-29 22:51:12 -0600

    Change UpgradeStore to class

--- src/state/upgradeStore.ts
+++ src/state/upgradeStore.ts
@@ -1,15 +1,32 @@
-import { useState } from 'react'
+import { useState, Dispatch, SetStateAction } from 'react'
 
-const upgradeStore = (() => {
-	const [gathererLevel, setGathererLevel] = useState(0)
-	const upgradeGatherers = () => {
-		setGathererLevel(gathererLevel => gathererLevel + 1)
+class UpgradeStore {
+	private gathererLevel: number
+	private setGathererLevel: Dispatch<SetStateAction<number>>
+
+	constructor(public initialLevel = 0) {
+		const [ _gathererLevel, setGathererLevel ] = useState(initialLevel)
+		this.setGathererLevel = setGathererLevel
+		this.gathererLevel = _gathererLevel
 	}
 
-	return {
-		gathererLevel,
-		upgradeGatherers
+	public getGathererLevel(): number {
+		return this.gathererLevel
 	}
-})()
 
-export { upgradeStore }
+	public upgradeGatherers(): void {
+		this.setGathererLevel(gathererLevel => gathererLevel + 1)
+	}
+
+	// const [gathererLevel, setGathererLevel] = useState(0)
+	// const upgradeGatherers = () => {
+	// 	setGathererLevel(gathererLevel => gathererLevel + 1)
+	// }
+
+	// return {
+	// 	gathererLevel,
+	// 	upgradeGatherers
+	// }
+}
+
+export { UpgradeStore }
