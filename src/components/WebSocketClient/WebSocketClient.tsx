c4bf65bde5ffbb9475a7ad9e2b2e841dadd56d4c
Anthony Williams <anthony.williams.cs@gmail.com>
2019-06-18 22:16:25 -0600

    Add log display for web socket client

--- src/components/WebSocketClient/WebSocketClient.tsx
+++ src/components/WebSocketClient/WebSocketClient.tsx
@@ -1,18 +1,26 @@
-import React, { FC, useEffect } from "react"
+import React, { FC, useEffect, useState } from "react"
 
 import './WebSocketClient.scss'
 
 const WebSocketClient: FC = () => {
+	const [logLines, setLogLines] = useState<string[]>([])
+
+	const appendToLog = (msg: string) => {
+		setLogLines(logLines => logLines.concat(msg))
+	}
+
 	// NOTE: This happens before un-render (only once)
 	const handleUnmount = () => {}
 
 	// NOTE: This happens after render (only once)
 	const handleMounted = () => {
+		appendToLog('component mounted')
 		// NOTE: create socket to echo server (use secure protocol, i.e. `wss` not `ws`)
 		const secureWebSocketProto = 'wss://'
 		const webSocketClient = new WebSocket(`${secureWebSocketProto}localhost:5001/ws`)
 
 		webSocketClient.onopen = evt => {
+			appendToLog('web socket connection opened')
 			const webSocketTarget: WebSocket = evt.target as WebSocket
 			console.log('web socket is opened! sending message to server...')
 			// NOTE: send message to server
@@ -20,6 +28,7 @@ const WebSocketClient: FC = () => {
 		}
 
 		webSocketClient.onmessage = evt => {
+			appendToLog('web socket message received')
 			const { data, target, type } = evt
 			const webSocketTarget: WebSocket = target as WebSocket
 			console.log(`message received from="${webSocketTarget.url}" type="${type}" data="${data}"`)
@@ -35,7 +44,16 @@ const WebSocketClient: FC = () => {
 		<article>
 			<h1>Web Socket Client</h1>
 			<section>
-				
+				<h2>Log</h2>
+				{/*
+				<textarea cols={80} rows={12} value={logLines} />
+				*/}
+				<textarea cols={80} rows={12} value={logLines.join('\n')} />
+				{/*
+				*/}
+				{/*
+				<textarea cols={80} rows={12} value={logLines.map(logLine => `${logLine}\n`)} />
+				*/}
 			</section>
 		</article>
 	)
