f2d6688f3fc873016297c84fd9f1f434aff22959
Anthony Williams <anthony.williams.cs@gmail.com>
2019-06-19 22:27:31 -0600

    Add ability to send message to server

--- src/components/WebSocketClient/WebSocketClient.tsx
+++ src/components/WebSocketClient/WebSocketClient.tsx
@@ -1,9 +1,24 @@
-import React, { FC, useEffect, useState } from "react"
+import React, { ChangeEvent, FC, useEffect, useState } from "react"
 
 import './WebSocketClient.scss'
 
+// NOTE: create socket to echo server (use secure protocol, i.e. `wss` not `ws`)
+const secureWebSocketProto = 'wss://'
+const webSocketClient = new WebSocket(`${secureWebSocketProto}localhost:5001/ws`)
+
+webSocketClient.onopen = evt => {
+	console.info('web socket is opened! sending message to server...')
+	// appendToLog('web socket is opened! sending message to server...')
+	const webSocketTarget: WebSocket = evt.target as WebSocket
+	// NOTE: send message to server
+	webSocketTarget.send('ello!!!')
+}
+
 const WebSocketClient: FC = () => {
+	const [currentMessage, setCurrentMessage] = useState('')
 	const [logLines, setLogLines] = useState<string[]>([])
+	// const [webSocketClient, setWebSocketClient] = useState<WebSocket>()
+	// let webSocketClient: WebSocket
 
 	const appendToLog = (msg: string, emitToConsole = true) => {
 		if (emitToConsole) {
@@ -13,21 +28,20 @@ const WebSocketClient: FC = () => {
 	}
 
 	// NOTE: This happens before un-render (only once)
-	const handleUnmount = () => {}
+	const handleUnmount = () => {
+		if (!webSocketClient) {
+			return
+		}
+		appendToLog('closing web socket...')
+		webSocketClient.close()
+	}
 
 	// NOTE: This happens after render (only once)
 	const handleMounted = () => {
 		appendToLog('component mounted')
 		// NOTE: create socket to echo server (use secure protocol, i.e. `wss` not `ws`)
-		const secureWebSocketProto = 'wss://'
-		const webSocketClient = new WebSocket(`${secureWebSocketProto}localhost:5001/ws`)
-
-		webSocketClient.onopen = evt => {
-			appendToLog('web socket is opened! sending message to server...')
-			const webSocketTarget: WebSocket = evt.target as WebSocket
-			// NOTE: send message to server
-			webSocketTarget.send('ello!!!')
-		}
+		// const secureWebSocketProto = 'wss://'
+		// webSocketClient = new WebSocket(`${secureWebSocketProto}localhost:5001/ws`)
 
 		webSocketClient.onmessage = evt => {
 			const { data, target, type } = evt
@@ -38,6 +52,22 @@ const WebSocketClient: FC = () => {
 		return handleUnmount
 	}
 
+	const handleCurrentMessageChange = (evt: ChangeEvent<HTMLInputElement>) => {
+		const { target } = evt
+		const { value } = target
+		setCurrentMessage(value)
+	}
+
+	const handleSendToServer = () => {
+		appendToLog(`sending message to server=${currentMessage}`)
+		if (!webSocketClient) {
+			console.warn('web socket client not available', webSocketClient)
+			return
+		}
+		webSocketClient.send(currentMessage)
+		setCurrentMessage('')
+	}
+
 	// NOTE: empty (no arg) to track nothing, just fire on mount/unmount
 	useEffect(handleMounted, [])
 
@@ -46,15 +76,16 @@ const WebSocketClient: FC = () => {
 			<h1>Web Socket Client</h1>
 			<section>
 				<h2>Log</h2>
-				{/*
-				<textarea cols={80} rows={12} value={logLines} />
-				*/}
-				<textarea cols={80} rows={12} value={logLines.join('\n')} />
-				{/*
-				*/}
-				{/*
-				<textarea cols={80} rows={12} value={logLines.map(logLine => `${logLine}\n`)} />
-				*/}
+				{/* <textarea cols={80} rows={12} value={logLines} /> */}
+				{/* <textarea cols={80} rows={12} value={logLines.map(logLine => `${logLine}\n`)} /> */}
+				<textarea cols={80} rows={12} value={logLines.join('\n')} readOnly={true} />
+			</section>
+			<section>
+				<input type="text" value={currentMessage}
+					onChange={handleCurrentMessageChange}
+					placeholder="Enter message to send to server..." />
+				<br />
+				<button onClick={handleSendToServer}>Send</button>
 			</section>
 		</article>
 	)
