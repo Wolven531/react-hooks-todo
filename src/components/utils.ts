125eba05fddae9507f5e3c3b72fd992869cca37d
Anthony Williams <anthony.williams.cs@gmail.com>
2019-06-22 22:48:49 -0600

    Add utils file

---
+++ src/components/utils.ts
@@ -0,0 +1,11 @@
+const MONEY_FORMAT_OPTIONS: Intl.NumberFormatOptions = { currency: 'USD', maximumFractionDigits: 0, minimumFractionDigits: 0, style: 'currency' }
+
+/*
+	@param number amount - The numeric money amount
+	@returns string - The formatted version of the money
+ */
+const monify = (amount: number): string => {
+	return amount.toLocaleString('en-US', MONEY_FORMAT_OPTIONS)
+}
+
+export { monify }
