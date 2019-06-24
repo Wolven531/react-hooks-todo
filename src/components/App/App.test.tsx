8beae4c93b08429b26a26682b6a8aeb461828d1f
Anthony Williams <anthony.williams.cs@gmail.com>
2019-06-23 22:06:17 -0600

    Improve coverage of App.tsx to 💯

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -7,7 +7,7 @@ import {
 } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 // import { expect } from 'jasmine'
-import jest from 'jest'
+// import { spyOn } from 'jest'
 // import {
 // 	beforeEach,
 // 	describe,
@@ -24,9 +24,8 @@ import { WebSocketClient } from '../WebSocketClient/WebSocketClient'
 
 // views
 import { CritterList } from '../../views/CritterList/CritterList'
-import { Jest } from '@jest/environment';
 
-describe('Shallow App component', () => {
+describe('Shallow render App component', () => {
 	let wrapperApp: ShallowWrapper<FC>
 
 	beforeEach(() => {
@@ -34,7 +33,7 @@ describe('Shallow App component', () => {
 		wrapperApp = shallow(<App/>)
 	})
 
-	it('shallow renders without crashing', () => {
+	it('shallow renders WebSocketClient, MoneyControls, and CritterList', () => {
 		wrapperApp.update()
 		expect(wrapperApp.exists()).toBe(true)
 
@@ -49,30 +48,36 @@ describe('Shallow App component', () => {
 	})
 })
 
-// describe('Mount App component', () => {
-// 	let spyComponentDidMount
-// 	let wrapperApp: ReactWrapper<FC>
+describe('Mount and render App component', () => {
+	// let spyComponentDidMount
+	let wrapperApp: ReactWrapper<FC>
 
-// 	beforeEach(() => {
-// 		// NOTE: need mount (rather than shallow) so that stateless componentDidMount will run
-// 		// wrapperApp = mount(<App/>)
-// 		const app = <App/>
-// 		spyComponentDidMount = jest.spyOn(app, 'componentDidMount')
-// 		wrapperApp = mount(app)
-// 	})
+	beforeEach(() => {
+		// NOTE: need mount (rather than shallow) so that stateless componentDidMount will run
+		// wrapperApp = mount(<App/>)
+		const app = <App/>
+		// spyComponentDidMount = spyOn(app, 'componentDidMount')
+		wrapperApp = mount(app)
+	})
 
-// 	it('shallow renders without crashing', () => {
-// 		expect(spyComponentDidMount).toHaveBeenCalled()
-// 		// wrapperApp.update()
-// 		// expect(wrapperApp.exists()).toBe(true)
+	afterEach(() => {
+		wrapperApp.unmount()
+	})
 
-// 		// const webSocketClient = wrapperApp.find(WebSocketClient)
-// 		// expect(webSocketClient.exists()).toBe(true)
+	it('mounts and renders WebSocketClient, MoneyControls, and CritterList', () => {
+		// expect(spyComponentDidMount).toHaveBeenCalled()
+		wrapperApp.update()
+		expect(wrapperApp.exists()).toBe(true)
 
-// 		// const moneyControls = wrapperApp.find(MoneyControls)
-// 		// expect(moneyControls.exists()).toBe(true)
+		const webSocketClient = wrapperApp.find(WebSocketClient)
+		expect(webSocketClient.exists()).toBe(true)
 
-// 		// const critterList = wrapperApp.find(CritterList)
-// 		// expect(critterList.exists()).toBe(true)
-// 	})
-// })
+		const moneyControls = wrapperApp.find(MoneyControls)
+		expect(moneyControls.exists()).toBe(true)
+
+		const critterList = wrapperApp.find(CritterList)
+		expect(critterList.exists()).toBe(true)
+
+		expect(document.title).toBe('Critter Manager')
+	})
+})
