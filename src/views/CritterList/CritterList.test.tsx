ad20b50b8f48894fbff8cce58f8dd8825ebcc979
Anthony Williams <anthony.williams.cs@gmail.com>
2019-07-05 22:48:48 -0600

    Another attempt at using mocks

--- src/views/CritterList/CritterList.test.tsx
+++ src/views/CritterList/CritterList.test.tsx
@@ -8,9 +8,10 @@ import {
 import Adapter from 'enzyme-adapter-react-16'
 import React, { FC } from 'react'
 
-// import { useCritterState } from '../../state/useCritterState'
+// import { Critter } from '../../components/Critter/Critter'
 
-// import { Critter } from '../../model/Critter'
+import { Critter as CritterModel } from '../../model/Critter'
+
 import { CritterList, ICritterListProps } from './CritterList'
 
 configure({ adapter: new Adapter() })
@@ -35,17 +36,25 @@ describe('Shallow render CritterList', () => {
 })
 
 describe('Mount and render CritterList', () => {
+	const fakeCritters = [new CritterModel('critter 1', 10, 2, 1)]
 	let mockLoadFromStorage: jest.Mock
-	let mockedUseCritterState: jest.Mock
+	let mockedUseCritterState: () => { critters: CritterModel[], loadFromStorage: () => void }
 	let wrapperCritterListPage: ReactWrapper<FC<ICritterListProps>>
 
 	beforeEach(() => {
 		mockLoadFromStorage = jest.fn()
-		mockedUseCritterState = jest.fn(() => {
+		// mockedUseCritterState = jest.fn(() => {
+		// 	return {
+		// 		critters: fakeCritters,
+		// 		loadFromStorage: mockLoadFromStorage
+		// 	}
+		// })
+		mockedUseCritterState = () => {
 			return {
+				critters: fakeCritters,
 				loadFromStorage: mockLoadFromStorage
 			}
-		})
+		}
 		// mockedUseCritterState = jest.genMockFromModule('../../state/useCritterState')
 		// mockedUseCritterState.mockImplementation(jest.fn())
 		// jest.mock('../../state/useCritterState', () => {
@@ -57,21 +66,19 @@ describe('Mount and render CritterList', () => {
 		// })
 		// jest.mock('../../state/useCritterState')
 		// useCritterState = jest.fn()
-		jest.mock('../../state/useCritterState', () => {
-			return mockedUseCritterState
-		})
+		jest.mock('../../state/useCritterState', () => { return mockedUseCritterState })
 		wrapperCritterListPage = mount(<CritterList currentMoney={0} />)
 		wrapperCritterListPage.hasClass('critter-list')
 
 		const displayContainer = wrapperCritterListPage.find('.display-container')
 		expect(displayContainer.exists()).toBe(true)
-		expect(displayContainer.children()).toHaveLength(0)
 	})
 
 	it('mounts and renders CritterList w/ no issues', () => {
 		wrapperCritterListPage.update()
 
 		expect(wrapperCritterListPage.exists()).toBe(true)
+		// expect(wrapperCritterListPage.find(Critter)).toHaveLength(fakeCritters.length)
 		// expect(jest.isMockFunction(useCritterState)).toBe(true)
 		// expect(mockLoadFromStorage).toHaveBeenCalledTimes(1)
 		// expect(mockedUseCritterState).toHaveBeenCalledTimes(1)
