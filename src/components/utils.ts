aeb4c9701362e8315b0e82a7dbb0da73190f269a
Anthony Williams <anthony.williams.cs@gmail.com>
2019-07-06 23:26:29 -0600

    Add prettifyTimestamp in utils

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -12,10 +12,22 @@ const DATE_FORMAT_OPTIONS: Intl.DateTimeFormatOptions = {
 const DATE_FORMATTER = new Intl.DateTimeFormat('en-US', DATE_FORMAT_OPTIONS)
 const MONEY_FORMAT_OPTIONS: Intl.NumberFormatOptions = { currency: 'USD', maximumFractionDigits: 0, minimumFractionDigits: 0, style: 'currency' }
 
+// const rtf = new Intl.RelativeTimeFormat('en', {
+// 	localeMatcher: 'best fit',// other values: 'lookup'
+// 	numeric: 'always',// other values: 'auto'
+// 	style: 'long'// other values: 'short' or 'narrow'
+// })
+
 /*
 	@param number amount - The numeric money amount
 	@returns string - The formatted version of the money
  */
 const monify = (amount: number): string => amount.toLocaleString('en-US', MONEY_FORMAT_OPTIONS)
 
-export { monify }
+/*
+	@param number timestamp - The numeric timestamp to convert
+	@returns string - The formatted version of the timestamp using DATE_FORMATTER
+ */
+const prettifyTimestamp = (timestamp: number): string => DATE_FORMATTER.format(timestamp)
+
+export { monify, prettifyTimestamp }
