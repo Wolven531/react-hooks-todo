3e5914f0c51287eb4b7e4d3b077b9cde21c8e755
Anthony Williams <anthony.williams.cs@gmail.com>
2019-07-13 22:09:28 -0600

    Add TodoForm assertions for TodoList unit test

--- src/components/TodoList/TodoList.test.tsx
+++ src/components/TodoList/TodoList.test.tsx
@@ -7,22 +7,31 @@ import Adapter from 'enzyme-adapter-react-16'
 import React, { FC } from 'react'
 
 import { ITodoListProps, TodoList } from './TodoList'
+import { TodoForm } from '../TodoForm/TodoForm';
 
 configure({ adapter: new Adapter() })
 
 describe('Shallow render TodoList component', () => {
+	let mockAddTodo: jest.Mock
 	let wrapperTodoList: ShallowWrapper<FC<ITodoListProps>>
 
 	beforeEach(() => {
+		mockAddTodo = jest.fn()
 		wrapperTodoList = shallow(<TodoList
-			addTodo={jest.fn()}
+			addTodo={mockAddTodo}
 			clearCompletedTodos={jest.fn()}
 			todos={[]}
 			toggleTodo={jest.fn()} />)
 		wrapperTodoList.update()
 	})
 
-	it('shallow renders properly', () => {
+	it('shallow renders properly, including TodoForm', () => {
 		expect(wrapperTodoList.exists()).toBe(true)
+
+		expect(wrapperTodoList.hasClass('todo-list')).toBe(true)
+
+		const todoForm = wrapperTodoList.find(TodoForm)
+		expect(todoForm.exists()).toBe(true)
+		expect(todoForm.props().addTodo).toEqual(mockAddTodo)
 	})
 })
